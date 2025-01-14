41e9d642bc531692a25befc6110bad60e1dd2162
Khattab <maaastafakhatab200@gmail.com>
2025-01-14 18:27:10 +0200

    Initial commit

---
+++ The_Trail.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+void get_shit_done() {
+    cout<<"YEs"<<endl;
+
+
+}
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        get_shit_done();
+
+    }
+}
